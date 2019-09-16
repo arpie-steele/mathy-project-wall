402cf0591a802882d4882676f58a7823132d0d59
Richard Penner <richardpenner@gmail.com>
2019-09-16 04:26:31 -0700

    perl tidy pass

--- src/MSC2010/03/logic-loop/perl/bootstrap.pl
+++ src/MSC2010/03/logic-loop/perl/bootstrap.pl
@@ -1,12 +1,14 @@
 my $bits_per_octet = length unpack 'B*', pack 'B', 0;
-my $id = 0;
-my $width = 1;
+my $id             = 0;
+my $width          = 1;
 
 while ( $width < $bits_per_octet ) {
-	my $bit_pattern = ( ( '0' x $width )  .  ( '1' x $width ) ) x ($bits_per_octet / ( 2 * $width ));
-        my $code = sprintf '\\0x%02X', ord pack "B$bits_per_octet", $bit_pattern;
-	print $id, ': ', $bit_pattern, ': ', $code , "\n";
-	$id ++;
-	$width *= 2;
+    my $bit_pattern =
+      ( ( '0' x $width ) . ( '1' x $width ) ) x
+      ( $bits_per_octet / ( 2 * $width ) );
+    my $code = sprintf '\\0x%02X', ord pack "B$bits_per_octet", $bit_pattern;
+    print $id, ': ', $bit_pattern, ': ', $code, "\n";
+    $id++;
+    $width *= 2;
 }
 
--- src/MSC2010/metamath_parser/perl/Metamath/Options.pm
+++ src/MSC2010/metamath_parser/perl/Metamath/Options.pm
@@ -7,28 +7,27 @@ use warnings;
 # loose options are options less restrictive than the spec.
 
 sub new {
-    my ($class_or_ref, %more_options) = @_;
+    my ( $class_or_ref, %more_options ) = @_;
     my $class = ref $class_or_ref || $class_or_ref;
-    my %options = $class->default_options() ;
+    my %options = $class->default_options();
     if ( ref $class_or_ref ) {
-	foreach my $key ( keys %{$class_or_ref} ) {
-	    my $value = $class_or_ref->{$key};
-	    if ( $value && $value eq 'UNSET' ) { delete $options{$key} ; next; }
-	    $options{$key} = $value;
-	}
+        foreach my $key ( keys %{$class_or_ref} ) {
+            my $value = $class_or_ref->{$key};
+            if ( $value && $value eq 'UNSET' ) { delete $options{$key}; next; }
+            $options{$key} = $value;
+        }
     }
     foreach my $key ( keys %more_options ) {
         my $value = $more_options{$key};
-        if ( $value && $value eq 'UNSET' ) { delete $options{$key} ; next; }
+        if ( $value && $value eq 'UNSET' ) { delete $options{$key}; next; }
         $options{$key} = $value;
     }
-    return bless { %options }, $class;
+    return bless {%options}, $class;
 }
 
 sub default_options {
     my ($class_or_ref) = @_;
-    my %options = (
-    );
+    my %options = ();
     return %options;
 }
 
