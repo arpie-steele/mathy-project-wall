5301318a3f0b62a7385ed8d6d0e403dbb8c964da
Richard Penner <richardpenner@gmail.com>
2019-09-29 21:52:33 -0700

    Started tokenization classification

--- src/MSC2010/metamath_parser/perl/Metamath/Token.pm
+++ src/MSC2010/metamath_parser/perl/Metamath/Token.pm
@@ -77,7 +77,8 @@ sub dump {
     my $fn = $self->get_fn();
     my $ln = $self->get_ln();
     my $str = $self->get_string();
-    print "$fn line $ln : $str\n";
+    my $type = $self->get_types();
+    print "$fn line $ln : $type $str\n";
 }
 
 sub dump_ws {
@@ -89,6 +90,82 @@ sub dump_ws {
     my $nl = 0;
     foreach my $c ( split //xms, $str ) { if ($c eq "\n") { $nl ++; } }
     print "$fn line $ln : WS length = $len, NL = $nl\n";
+}
+
+sub get_types {
+    my ($self) = @_;
+    my %types;
+    if ( exists $self->{'types'} ) {
+        %types = %{$self->{'types'}};
+    } elsif ( $self->is_ws() ) {
+        $types{'*'} = 'WS';
+        $self->{'types'} = \%types;
+    } else {
+        my $str = $self->get_string();
+        if ( $str =~ /\A[\w.-]+\z/xms ) {
+	    $types{'TOP'} = 'LABEL';
+	    $types{'BODY'} = 'LABEL';
+	    $types{'LABEL'} = 'LABEL';
+	    $types{'FILENAME'} = 'FILENAME';
+	    $types{'COMMENT'} = 'COMMENT';
+	    $types{'MATH'} = 'SYMBOL';
+        } elsif ( $str =~ /\A[^[:^graph:]\$]+\z/xms ) {
+	    $types{'FILENAME'} = 'FILENAME';
+	    $types{'COMMENT'} = 'COMMENT';
+	    $types{'MATH'} = 'SYMBOL';
+	}
+        if ( $str eq q{$(} ) {
+	    $types{'TOP'} = 'BEGIN_COMMENT';
+	    $types{'BODY'} = 'BEGIN_COMMENT';
+	    $types{'*'} = 'BEGIN_COMMENT';
+        }
+        if ( $str eq q(${) ) {
+	    $types{'TOP'} = 'BEGIN_BLOCK';
+	    $types{'BODY'} = 'BEGIN_BLOCK';
+        }
+        if ( $str eq q($}) ) {
+	    $types{'TOP'} = 'END_BLOCK';
+	    $types{'BODY'} = 'END_BLOCK';
+        }
+        if ( $str eq q{$[} ) {
+	    $types{'TOP'} = 'BEGIN_FILE_INCLUDE';
+        }
+        if ( $str eq q{$]} ) {
+	    $types{'TOP'} = 'END_FILE_INCLUDE';
+        }
+        if ( $str eq q{$t} ) {
+	    $types{'COMMENT'} = 'TYPESETTING';
+        }
+        if ( $str =~ /\A[\#]{4,}\z/ ) {
+	    $types{'COMMENT'} = 'H1MARKER';
+        }
+        if ( $str =~ /\A(:?[\#][*]){2,}[\#]?\z/ ) {
+	    $types{'COMMENT'} = 'H2MARKER';
+        }
+        if ( $str =~ /\A(:?[=][-]){2,}[=]?\z/ ) {
+	    $types{'COMMENT'} = 'H3MARKER';
+        }
+        if ( $str =~ /\A(:?[-][.]){2,}[-]?\z/ ) {
+	    $types{'COMMENT'} = 'H4MARKER';
+        }
+        if ( $str eq q{$)} ) {
+	    %types = ();
+	    $types{'COMMENT'} = 'END_COMMENT';
+	    $types{'*'} = 'ERROR';
+        }
+        if ( ! exists $types{'*'} ) {
+	    $types{'*'} = 'UNKNOWN';
+        }
+    }
+    if ( wantarray ) {
+        my @pairs = map { ( $_ => $types{$_} ) ; } reverse sort keys %types;
+	return @pairs;
+    }
+    if ( 1 == scalar keys %types ) {
+	return (values %types)[0];
+    }
+    my $strval = join q(, ), map { "$_ => $types{$_}" } reverse sort keys %types;
+    return "($strval)";
 }
 
 1;
